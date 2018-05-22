a1e88e2418a4d915d6f6262a09e0c102305da6d1
assig <assigdev@gmail.com>
2018-05-22 17:33:36 +0300

    add altstartapp managment command , add license

---
+++ apps/main/management/commands/altstartapp.py
@@ -0,0 +1,20 @@
+from django.core.management.commands import startapp
+
+
+APP_TEMPLATES = {
+    'full': 'https://github.com/assigdev/django_alt_start_app_template/archive/full.zip',
+    'default': 'https://github.com/assigdev/django_alt_start_app_template/archive/master.zip',
+}
+
+
+class Command(startapp.Command):
+    help = "Alternative start app 'app_name' or 'app_name --type=full'"
+
+    def add_arguments(self, parser):
+        parser.add_argument('type', nargs='+')
+
+    def handle(self, *args, **options):
+        app_type = options.get('type', 'default')
+        options['template'] = APP_TEMPLATES[app_type]
+        options['extensions'] = 'py,html'
+        super().handle(**options)
